90f7ad13c158d7737ad3a6445aba478159fa2dd6
jkasato <jasato120490@gmail.com>
2019-08-13 22:19:01 -0700

    moved routes and functions to admin folder

---
+++ app/functions.js
@@ -0,0 +1,40 @@
+/**
+ * Checks the bcrypt value of the password submitted
+ * @param {string} password
+ * @return {boolean} true if password submitted is equal to
+ * bcrypt-hashed value, false otherwise. 
+ */
+function checkPassword(password, hashedValue) {
+    return new Promise(function(resolve, reject) {
+        bcrypt.compare(password, hashedValue, function(err, result) {
+            console.log("Result: " + result);
+            resolve(result);
+        });
+    });
+}
+/**
+ * This function checks if the username is in the database
+ * @param {string} username 
+ */
+function checkUsername(username) {
+    let sql = "SELECT * FROM users WHERE username=?";
+    return new Promise(function(resolve, reject) {
+        let conn = createDBConnection();
+        conn.connect(function(err) {
+            if (err) throw err;
+            conn.query(sql, [username], function(err, rows, fields) {
+                if (err) throw err;
+                console.log("Rows found: " + rows.length);
+                resolve(rows);;
+            }); //query
+        }); //connect
+    }); //promise
+}
+
+function isAuthenticated(req, res, next) {
+    if (!req.session.authenticated) {
+        res.redirect('common/views/login.njk');
+    } else {
+        next()
+    }
+}
--- app/routes.js
+++ app/routes.js
@@ -18,7 +18,7 @@ router.use('/app', express.static('app/public'));
 
 // app routes
 router.get('/',
-    async (req, res) => res.render('app/root.njk', {
+    async(req, res) => res.render('app/root.njk', {
         titles: await dbGet(SQL.titles),
         types: await dbGet(SQL.types),
         materials: await dbGet(SQL.materials),
@@ -26,9 +26,53 @@ router.get('/',
     }));
 
 // catch all
-router.get('/*', function (req, res) {
+router.get('/*', function(req, res) {
     return res.render('404');
 });
 
+//routes
+server.use(express.urlencoded({ extended: true }));
+
+
+server.post("login", async function(req, res) {
+    let username = req.body.username;
+    let password = req.body.password;
+
+    //checks if username is in the database
+    let result = await checkUsername(username);
+    console.dir(result);
+
+    // let hashedPwd = "$2a$05$r2tnsPeQXP/yWuh7.Mz3MO3zkUAposLywMXrsQ1EFZpf2ecvzw6mm";
+    //initialize pashedPwd to blank
+    let hashedPwd = "";
+
+    //check if result is in the database
+    if (result.length > 0) {
+        hashedPwd = result[0].password;
+    }
+
+    let passwordMatch = await checkPassword(password, hashedPwd);
+    console.log("passwordMatch: " + passwordMatch);
+
+    if (username == 'admin' && passwordMatch) {
+        req.session.authenticated = true;
+        res.render("admin");
+    } else {
+        res.render("login", { "loginError": true });
+    }
+});
+
+server.get("/myAccount", isAuthenticated, function(req, res) {
+    if (req.session.authenticated) {
+        res.render("admin");
+    } else {
+        res.redirect("login");
+    }
+});
+
+server.get("/logout", function(req, res) {
+    req.session.destroy();
+    res.redirect("login");
+});
 
 module.exports = router;

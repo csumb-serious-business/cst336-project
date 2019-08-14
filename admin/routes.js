72ed3a872552f22f83a8ed0270d12211c0258ee3
jkasato <jasato120490@gmail.com>
2019-08-14 01:30:34 -0700

    removed some functions that dealt with authentication

--- admin/routes.js
+++ admin/routes.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
+const app = require("../app.js");
+var server = express();
+server.set('view engine', 'njk');
+
 //serve static files
 router.use('/admin', express.static('admin/public'));
+
+const tools = require("./functions");
+
 
 /* app routes
  * DON'T USE THESE PATHS:
@@ -14,7 +21,6 @@ router.get('/admin',
         /* todo add app forms */
         example: await { /* some slow callback */ }
     }));
-0
 
 // example of passing a function instead of anonymous
 router.get('/example', example);
@@ -23,5 +29,49 @@ function example(req, res) {
     // do something here if required, ie check for redirects
     return res.render('admin/root.njk', {});
 }
+//routes
+server.use(express.urlencoded({ extended: true }));
 
+
+server.post("login", async function(req, res) {
+    let username = req.body.username;
+    let password = req.body.password;
+
+    //checks if username is in the database
+    // let result = await checkUsername(username);
+    // console.dir(result);
+
+    //     // // let hashedPwd = "$2a$05$r2tnsPeQXP/yWuh7.Mz3MO3zkUAposLywMXrsQ1EFZpf2ecvzw6mm";
+    //     // //initialize pashedPwd to blank
+    //     // let hashedPwd = "";
+
+    //     // //check if result is in the database
+    //     // if (result.length > 0) {
+    //     //     hashedPwd = result[0].password;
+    //     // }
+
+    //     // let passwordMatch = await checkPassword(password, hashedPwd);
+    //     // console.log("passwordMatch: " + passwordMatch);
+
+    //     // if (username == 'admin' && passwordMatch) {
+    if (username == 'admin' && username == 'secret') {
+        req.session.authenticated = true;
+        res.render("admin");
+    } else {
+        res.render("login", { "loginError": true });
+    }
+});
+
+// server.get("/myAccount", tools.isAuthenticated, function(req, res) {
+//     if (req.session.authenticated) {
+//         res.render("admin");
+//     } else {
+//         res.redirect("login");
+//     }
+// });
+
+server.get("/logout", function(req, res) {
+    req.session.destroy();
+    res.redirect("login");
+});
 module.exports = router;

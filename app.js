2de31845f1b0df272dc1b1f067be7711456655e9
jkasato <jasato120490@gmail.com>
2019-08-16 00:41:32 -0700

    State changed from starting to crashed

--- app.js
+++ app.js
@@ -32,6 +32,6 @@ server.get('/*', function(req, res) {
 
 
 var port = 33333;
-// server.listen(port);
-server.listen(process.env.PORT || 5000);
+server.listen(port);
+// server.listen(process.env.PORT || 5000);
 console.log(`server listening on port ${port}`);
